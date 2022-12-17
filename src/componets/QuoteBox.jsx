5533e02fbd4d4e35fe553a2ce7d2deeb29992506
ingIsaiasHernandez <ing.isaias.hdez@gmail.com>
2022-12-17 03:50:06 -0600

    [add] - the random color was added

--- src/componets/QuoteBox.jsx
+++ src/componets/QuoteBox.jsx
@@ -8,14 +8,28 @@ const QuoteBox = () => {
 
 
     const index = Math.floor(Math.random() * comments.length);
+
     const [text, setText] = useState(comments[index]);
 
+
     const myCommits = () =>{
-        setText(Math.floor(Math.random() * comments.length))
+        setText(Math.floor(Math.random() * comments.length));
+
     }
 
+    const colors = [
+        "#845EC2",
+        "#D65DB1",
+        "#FF6F91",
+        "#FF9671",
+        "#FFC75F",
+        "#F9F871"
+      ];
 
+    const colorIndex = Math.floor(Math.random() * colors.length);
 
+    
+    document.body.style = `background: ${colors[colorIndex]}`;
 
     return (
         <div className='card-container'>
@@ -24,15 +38,15 @@ const QuoteBox = () => {
 
                 <div className='card-letter'>
                     <UilCommentAltQuestion className="message-icon"/>   
-                    <h3 className='card-text'>{comments[index].quote}</h3>
+                    <h3 className='card-text' style={{ color: colors[colorIndex] }}>{comments[index].quote}</h3>
                 </div>
 
 
-                    <h4 className='card-text-secundary'>{comments[index].author}</h4>
+                    <h4 className='card-text-secundary' style={{ color: colors[colorIndex] }}>Author - {comments[index].author}</h4>
 
                 <div className='card-content-btn'>
 
-                    <button onClick={myCommits} className='card-btn'>Click to change <UilAngleRightB className="send-icon"/>   
+                    <button onClick={myCommits} className='card-btn' style={{ color: colors[colorIndex] }}>Click to change <UilAngleRightB className="send-icon"/>   
                     </button>
 
                 </div>
